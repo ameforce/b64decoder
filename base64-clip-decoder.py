a94a97d009acd069ea1dc8b2e23d38b8458d8526
jongin.kim <enmsoftware@gmail.com>
2022-08-23 15:28:51 +0900

    added clipboard monitoring functionality
    Added clipboard monitoring.
    You must now enter monitor_interval of float type when calling the constructor of ENMBase64.

--- base64-clip-decoder.py
+++ base64-clip-decoder.py
@@ -1,16 +1,22 @@
+import threading
+import pyperclip
 import binascii
 import base64
-import string
 
 
 class ENMBase64:
-    @staticmethod
-    def is_base64(resource: string):
+    def __init__(self, monitor_interval: float = 0.5):
+        self.monitor_interval = monitor_interval
+        self.latest_resource = ''
+        self.timer = None
+        self.start_timer()
+
+    def is_base64(self):
         try:
-            if isinstance(resource, str):
-                resource_bytes = bytes(resource, 'ascii')
-            elif isinstance(resource, bytes):
-                resource_bytes = resource
+            if isinstance(self.latest_resource, str):
+                resource_bytes = bytes(self.latest_resource, 'ascii')
+            elif isinstance(self.latest_resource, bytes):
+                resource_bytes = self.latest_resource
             else:
                 raise ValueError("Argument must be string or bytes")
             return base64.b64encode(base64.b64decode(resource_bytes)) == resource_bytes
@@ -19,9 +25,26 @@ class ENMBase64:
         except UnicodeDecodeError:
             return False
 
-    def decode_base64(self, resource: string):
-        resource_state = self.is_base64(resource)
+    def decode_base64(self):
+        resource_state = self.is_base64()
         convert_resource = ''
         if resource_state:
-            convert_resource = base64.b64decode(resource).decode('ascii')
+            convert_resource = base64.b64decode(self.latest_resource).decode('ascii')
         return resource_state, convert_resource
+
+    def clip_monitoring(self):
+        print('check')
+        clip_resource = pyperclip.paste()
+        if clip_resource != self.latest_resource:
+            self.latest_resource = clip_resource
+            self.decode_base64()
+        self.start_timer()
+
+    def start_timer(self):
+        self.timer = threading.Timer(self.monitor_interval, self.clip_monitoring)
+        self.timer.start()
+
+    def stop_monitoring(self):
+        if self.timer is None:
+            return False
+        self.timer.cancle()
